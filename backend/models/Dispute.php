c6f3b28453f304e09cd26c8d85b5bb161de2795c
khodos <artemkhodos@gmail.com>
2017-11-13 12:36:08 +0200

    d models

--- backend/models/Dispute.php
+++ backend/models/Dispute.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace backend\models;
 
 use Yii;
@@ -18,42 +19,45 @@ use yii\db\ActiveRecord;
  * @property string $closed
  * @property string $closed_by
  */
-class Dispute extends ActiveRecord {
+class Dispute extends ActiveRecord
+{
 
     const STATUS_IN_PROGRESS = 0;
-    const STATUS_CLOSED_WIN = 1;
+    const STATUS_CLOSED_WIN  = 1;
     const STATUS_CLOSED_LOSS = 2;
 
     /**
      * @return string the associated database table name
      */
-    public static function tableName() {
+    public static function tableName()
+    {
         return 'dispute';
     }
 
     /**
      * @return array validation rules for model attributes.
      */
-    public function rules() {
-        return array(
-            array('ref, status, added, added_by', 'required'),
-            array('status', 'numerical', 'integerOnly' => true),
-            array('ref', 'length', 'max' => 255),
-            array('added_by, closed_by', 'length', 'max' => 11),
-            array('closed', 'safe'),
-            array('id, ref, status, added, added_by, closed, closed_by', 'safe', 'on' => 'search'),
-        );
+    public function rules()
+    {
+        return [
+            ['ref, status, added, added_by', 'required'],
+            ['status', 'numerical', 'integerOnly' => true],
+            ['ref', 'length', 'max' => 255],
+            ['added_by, closed_by', 'length', 'max' => 11],
+            ['closed', 'safe'],
+            ['id, ref, status, added, added_by, closed, closed_by', 'safe', 'on' => 'search'],
+        ];
     }
-    
-    public function beforeValidate() {
+
+    public function beforeValidate()
+    {
         // create token and datetime for new records
         if ($this->isNewRecord) {
-            $this->status = self::STATUS_IN_PROGRESS;
-            $this->added = date('Y-m-d H:i:s');
+            $this->status   = self::STATUS_IN_PROGRESS;
+            $this->added    = date('Y-m-d H:i:s');
             $this->added_by = Yii::$app->getUser()->getId();
-        }
-        else if($this->status > 0) {    //  && empty($this->closed)
-            $this->closed = date('Y-m-d H:i:s');
+        } elseif ($this->status > 0) {    //  && empty($this->closed)
+            $this->closed    = date('Y-m-d H:i:s');
             $this->closed_by = Yii::$app->getUser()->getId();
         }
 
@@ -63,23 +67,25 @@ class Dispute extends ActiveRecord {
     /**
      * @return array customized attribute labels (name=>label)
      */
-    public function attributeLabels() {
-        return array(
-            'id' => 'ID',
-            'ref' => 'Ref',
-            'status' => 'Status',
-            'added' => 'Added',
-            'added_by' => 'Added By',
-            'closed' => 'Closed',
+    public function attributeLabels()
+    {
+        return [
+            'id'        => 'ID',
+            'ref'       => 'Ref',
+            'status'    => 'Status',
+            'added'     => 'Added',
+            'added_by'  => 'Added By',
+            'closed'    => 'Closed',
             'closed_by' => 'Closed By',
-        );
+        ];
     }
 
     /**
      * Retrieves a list of models based on the current search/filter conditions.
      * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
      */
-    public function search() {
+    public function search()
+    {
 
         $query = new ActiveQuery();
 
@@ -91,9 +97,9 @@ class Dispute extends ActiveRecord {
         $query->compare(['like', 'closed', $this->closed]);
         $query->compare(['like', 'closed_by', $this->closed_by]);
 
-        return new ActiveDataProvider($this, array(
-                    'query' => $query,
-                ));
+        return new ActiveDataProvider($this, [
+            'query' => $query,
+        ]);
     }
 
 }
