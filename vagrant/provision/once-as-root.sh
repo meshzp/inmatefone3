e66a0f504c786da33e584dee7e86f584c5949903
khodos <artemkhodos@gmail.com>
2017-11-14 12:52:00 +0200

    Vagrant configuring

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -16,37 +16,27 @@ function info {
 
 info "Provision-script user: `whoami`"
 
-export DEBIAN_FRONTEND=noninteractive
-
 info "Configure timezone"
 timedatectl set-timezone ${timezone} --no-ask-password
 
-info "Prepare root password for MySQL"
-debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password \"''\""
-debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password \"''\""
-echo "Done!"
-
 info "Update OS software"
-apt-get update
-apt-get upgrade -y
+yum upgrade -y
+
+info "Adding additional repos"
+ln -s /app/vagrant/etc/yum.repos.d/nginx.repo /etc/yum.repos.d/nginx.repo
+cd /tmp
+curl 'https://setup.ius.io/' -o setup-ius.sh
+bash setup-ius.sh
+echo "Done!"
 
 info "Install additional software"
-apt-get install -y php7.0-curl php7.0-cli php7.0-intl php7.0-mysqlnd php7.0-gd php7.0-fpm php7.0-mbstring php7.0-xml unzip nginx mysql-server-5.7 php.xdebug
-
-info "Configure MySQL"
-sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
-mysql -uroot <<< "CREATE USER 'root'@'%' IDENTIFIED BY ''"
-mysql -uroot <<< "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'"
-mysql -uroot <<< "DROP USER 'root'@'localhost'"
-mysql -uroot <<< "FLUSH PRIVILEGES"
-echo "Done!"
+yum install -y git mc wget epel-release
+yum install -y php71u-bcmath php71u-cli php71u-common php71u-devel php71u-fpm php71u-gd php71u-imap php71u-intl php71u-mbstring php71u-mcrypt php71u-mysqlnd php71u-pdo php71u-pecl-geoip php71u-pecl-imagick php71u-pecl-memcached php71u-pecl-redis php71u-pecl-xdebug php71u-soap php71u-xml unzip nginx mariadb mariadb-devel mariadb-server memcached memcached-devel redis
 
 info "Configure PHP-FPM"
-sed -i 's/user = www-data/user = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/group = www-data/group = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/owner = www-data/owner = vagrant/g' /etc/php/7.0/fpm/pool.d/www.conf
-cat << EOF > /etc/php/7.0/mods-available/xdebug.ini
-zend_extension=xdebug.so
+sed -i 's/user = php-fpm/user = nginx/g' /etc/php-fpm.d/www.conf
+sed -i 's/group = php-fpm/group = nginx/g' /etc/php-fpm.d/www.conf
+cat << EOF > /etc/php.d/xdebug.ini
 xdebug.remote_enable=1
 xdebug.remote_connect_back=1
 xdebug.remote_port=9000
@@ -54,18 +44,38 @@ xdebug.remote_autostart=1
 EOF
 echo "Done!"
 
-info "Configure NGINX"
-sed -i 's/user www-data/user vagrant/g' /etc/nginx/nginx.conf
+info "Configure SELinux"
+sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+setenforce 0
+usermod -a -G vagrant nginx
 echo "Done!"
 
 info "Enabling site configuration"
-ln -s /app/vagrant/nginx/app.conf /etc/nginx/sites-enabled/app.conf
+ln -s /app/vagrant/etc/nginx/conf.d/inmatefone3.backend.conf /etc/nginx/conf.d/inmatefone3.backend.conf
+ln -s /app/vagrant/etc/nginx/conf.d/inmatefone3.conf /etc/nginx/conf.d/inmatefone3.conf
+echo "Done!"
+
+info "Enabling services"
+systemctl enable nginx
+systemctl start nginx
+systemctl enable php-fpm
+systemctl start php-fpm
+systemctl enable mariadb
+systemctl start mariadb
+systemctl enable memcached
+systemctl start memcached
+systemctl enable redis
+systemctl start redis
 echo "Done!"
 
 info "Initailize databases for MySQL"
-mysql -uroot <<< "CREATE DATABASE yii2advanced"
-mysql -uroot <<< "CREATE DATABASE yii2advanced_test"
+mysql -uroot <<< "CREATE DATABASE inmatefone"
+mysql -uroot <<< "CREATE DATABASE inmatefone_test"
+mysql -uroot <<< "CREATE USER 'root'@'%' IDENTIFIED BY ''"
+mysql -uroot <<< "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'"
+mysql -uroot <<< "DROP USER 'root'@'localhost'"
+mysql -uroot <<< "FLUSH PRIVILEGES"
 echo "Done!"
 
 info "Install composer"
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -22,17 +22,13 @@ echo "Done!"
 
 info "Install project dependencies"
 cd /app
-composer --no-progress --prefer-dist install
+composer --no-progress --no-interaction --prefer-dist install
 
 info "Init project"
 ./init --env=Development --overwrite=y
 
 info "Apply migrations"
-./yii migrate --interactive=0
-./yii_test migrate --interactive=0
+./yii migrate <<< "yes"
 
 info "Create bash-alias 'app' for vagrant user"
-echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
-
-info "Enabling colorized prompt for guest console"
-sed -i "s/#force_color_prompt=yes/force_color_prompt=yes/" /home/vagrant/.bashrc
+echo 'alias app="cd /app"' >> /home/vagrant/.bashrc
